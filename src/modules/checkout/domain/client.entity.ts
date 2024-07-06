b4af2edbcd232330bf72c14a21f70789374af894
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2024-07-06 18:44:11 -0300

    implementing checkout

---
+++ src/modules/checkout/domain/client.entity.ts
@@ -0,0 +1,35 @@
+import { AggregateRoot, BaseEntity } from "../../@shared/domain/entity/";
+
+import { Id } from "../../@shared/domain/value-object";
+
+type ClientProps = {
+  id?: Id;
+  name: string;
+  email: string;
+  address: string;
+};
+
+export class Client extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _email: string;
+  private _address: string;
+
+  constructor(props: ClientProps) {
+    super(props.id);
+    this._name = props.name;
+    this._email = props.email;
+    this._address = props.address;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get address() {
+    return this._address;
+  }
+}
---
+++ src/modules/checkout/domain/product.entity.ts
@@ -0,0 +1,34 @@
+import { AggregateRoot, BaseEntity } from "../../@shared/domain/entity";
+import { Id } from "../../@shared/domain/value-object";
+
+type ProductProps = {
+  id?: Id;
+  name: string;
+  description: string;
+  salesPrice: number;
+};
+
+export class Product extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _description: string;
+  private _salesPrice: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this._name = props.name;
+    this._description = props.description;
+    this._salesPrice = props.salesPrice;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get salesPrice() {
+    return this._salesPrice;
+  }
+}
